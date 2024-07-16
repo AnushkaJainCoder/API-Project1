74137e9d0d0a45be37b2b99ac4c1753c9419e31e
Anushka Jain <102728955+Anushkajain22@users.noreply.github.com>
2024-07-16 20:00:12 +0530

    get users

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
 const express = require("express");
+const users = require("./MOCK_DATA.json")
 const app = express();
 const port = 8000;
+
+
+app.get('/users',(req,res)=>{
+    const html = `
+        <ul>
+            ${users.map(user =>
+                `<li> ${user.first_name}</li>`
+            ).join("")}
+        </ul>
+       
+    `;
+    res.send(html);
+})
+
+app.get('/api/users',(req,res)=>{
+    return res.json(users);
+})
+
+app.get("/api/users/:id",(req,res)=>{
+
+})
+
+
 
 app.listen(port,()=>console.log(`server started at ${port}`));
