932a263d62c8905aebff18b850f20380b0b80cec
Anushka Jain <102728955+Anushkajain22@users.noreply.github.com>
2024-07-16 20:03:40 +0530

    get user data

--- index.js
+++ index.js
@@ -21,7 +21,9 @@ app.get('/api/users',(req,res)=>{
 })
 
 app.get("/api/users/:id",(req,res)=>{
-
+    const id = Number(req.params.id);
+    const user = users.find(user => user.id === id);
+    return res.json(user);
 })
 
 
