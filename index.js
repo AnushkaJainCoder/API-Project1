ab46ff28501b46f985380b4d2f72eadbc325695a
Anushka Jain <102728955+Anushkajain22@users.noreply.github.com>
2024-07-16 20:13:17 +0530

    user data fetch update and delete

--- index.js
+++ index.js
@@ -20,11 +20,29 @@ app.get('/api/users',(req,res)=>{
     return res.json(users);
 })
 
-app.get("/api/users/:id",(req,res)=>{
+app.route("/api/users/:id").get((req,res)=>{
     const id = Number(req.params.id);
     const user = users.find(user => user.id === id);
     return res.json(user);
+}).put((req,res)=>{
+    const id = req.params.id;
+    return res.json({status: "pending"});
+}).delete((req,res)=>{
+    const id = req.params.id;
+    return res.json({status: "pending"});
 })
+
+// app.get("/api/users/:id",(req,res)=>{
+//     const id = Number(req.params.id);
+//     const user = users.find(user => user.id === id);
+//     return res.json(user);
+// })
+
+app.post("/api/users",(req,res)=>{
+    //to do 
+    res.json({status: "pending"});
+})
+
 
 
 
